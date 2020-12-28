337f4903a991049b4dfb152126b1df13462c28f3
Carrie Bares <caroltbares@gmail.com>
2020-12-28 04:45:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
   str_arr = str.split(" ")
-  new_str = (str_arr.each {|word| word.reverse}).join(" ")
+  str_arr.each {|word| word.reverse}).join(" ")
   return new_str
 end
